8966410f433ad1e486632f01865757c0e4653cef
Krissssssssssssssssss <kpavelov@iesjulioantonio.cat>
2025-10-03 12:36:32 +0200

    Update RA2_2.1_6_KrisPavelov.py

--- RA2_2.1_1_KrisPavelov/RA2_2.1_6_KrisPavelov.py
+++ RA2_2.1_1_KrisPavelov/RA2_2.1_6_KrisPavelov.py
@@ -5,10 +5,11 @@
 # Descripció: Plantilla per a scripts en bash
 # Especificació de la tasca: Fes un programa que demani una lletra i et digui si es vocal o consonant.
 
-lletra=input("Introdueix una lletra: ").lower()
+lletra = input("Introdueix una lletra: ")
+
 if lletra in 'aeiou':
-    print("La lletra és una vocal") 
-elif lletra.isalpha() and len(lletra)==1:
-    print("La lletra és una consonant") 
+    print("La lletra és una vocal")
+elif len(lletra) == 1:
+    print("La lletra és una consonant")
 else:
     print("Entrada invàlida")
